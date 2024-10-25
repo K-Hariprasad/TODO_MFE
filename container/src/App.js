0048ae8572e555dd8c5a36eb7c031c705cbf8c4b
Hariprasad <hariprasadk125@gmail.com>
2024-10-25 23:48:44 +0530

    included react import

--- container/src/App.js
+++ container/src/App.js
@@ -1,7 +1,7 @@
+import React from "react";
+
 function App() {
-  return (
-    <div>This is the container entry!!!!</div>
-  )
+  return <div>This is the container entry!!!!</div>;
 }
 
-export default App
+export default App;
