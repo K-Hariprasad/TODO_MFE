bceb852d4d8de03b359f2e24ee4603a9089c730c
Hariprasad <hariprasadk125@gmail.com>
2024-10-26 23:31:52 +0530

    config modified for routing

--- todo/webpack-config/webpack.dev.js
+++ todo/webpack-config/webpack.dev.js
@@ -1,14 +1,14 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { merge } = require("webpack-merge");
 const commonConfig = require("./webpack.common");
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const devConfig = {
   mode: "development",
   devServer: {
     port: 8081,
     historyApiFallback: {
-      index: "index.html",
+      index: "/index.html",
     },
   },
   plugins: [
@@ -16,14 +16,19 @@ const devConfig = {
       template: "./public/index.html",
     }),
     new ModuleFederationPlugin({
-      name : 'todo',
-      filename : 'remoteEntry.js',
-      exposes : {
+      name: "todo",
+      filename: "remoteEntry.js",
+      exposes: {
         //mount is exported from bootstrap not index
-        './todoIndex' : './src/bootstrap.js'
-      }
-    })
+        "./todoIndex": "./src/bootstrap.js",
+      },
+    }),
   ],
+  // this is required to load the nested routes directly eg: http://localhost:8081/product/5
+  // if below config not provided browser will make request to http://localhost:8081/product/remoteEntry.js
+  output: {
+    publicPath: "http://localhost:8081/",
+  },
 };
 
 module.exports = merge(commonConfig, devConfig);
